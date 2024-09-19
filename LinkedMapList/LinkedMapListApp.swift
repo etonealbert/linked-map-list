4d6188fcafd7abb18ab63af31a6ad7a2c2d24f7f
etonealbert <Albert.lukmanov99@gmail.com>
2024-09-19 08:56:17 -0300

    Initial Commit

---
+++ LinkedMapList/LinkedMapListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedMapListApp.swift
+//  LinkedMapList
+//
+//  Created by Albert Lukmanov on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct LinkedMapListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
